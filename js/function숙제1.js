6537dda737305ff5165e3b4f0a44d93fe2913ad3
김준현 <kjh31160694@gmail.com>
2025-04-28 17:49:00 +0900

    숙제 2까지

---
+++ js/function숙제1.js
@@ -0,0 +1,11 @@
+// function 숙제1.js
+
+max(100, 110);
+
+function max(num1, num2) {
+  if (num1 > num2) {
+    console.log(`${num1}이/가 더 큰수 입니다.`)
+  } else {
+    console.log(`${num2}이/가 더 큰수 입니다`)
+  }
+}
---
+++ js/function숙제2.js
@@ -0,0 +1,7 @@
+// function숙제2.js
+
+function sumUpTo (num1) {
+  for (i = 0; i <= num1; i++) {
+    let sum = result
+  }
+}
